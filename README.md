fa252f765b6f802db0adbdd8b2cec8ac4466be88
wookayin <wookayin@gmail.com>
2012-09-23 12:24:08 +0900

    initial writing of installation script and README

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-dotfiles
+Dotfiles
 ========
 
-personal dotfiles for linux systems
+Personal dotfiles for linux systems.
+
+## Installation
+
+### Clone and Install!
+
+```bash
+git clone https://github.com/wookayin/dotfiles.git ~/.dotfiles 
+cd ~/.dotfiles
+python install.py
+```
+
+The installation script will create symbolic links for the specified dotfiles.
+
+### install.py script
+
+This is a rough installation script written in python; 
+the task definition lies on the top of the script file.
---
+++ install.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+
+################# BEGIN OF FIXME #################
+
+# Task Definition
+# (path of target symlink) : (location of source file in the repository)
+
+tasks = {
+#	'~/.bashrc' : '.bashrc'
+}
+
+################# END OF FIXME #################
+
+
+import glob, os
+from optparse import OptionParser
+from sys import stderr
+
+# command line arguments
+def option():
+	parser = OptionParser()
+	parser.add_option("-f", "--force", action="store_true", default=False)
+	(options, args) = parser.parse_args()
+	return options
+
+# get current directory (absolute path) and options
+current_dir = os.path.abspath(os.path.dirname(__file__))
+options = option()
+
+for target, source in tasks.items():
+	# normalize paths
+	source = os.path.join(current_dir, source)
+	target = os.path.expanduser(target)
+
+	# if source does not exists...
+	if not os.path.lexists(source):
+		print >> stderr, ("source %s : does not exists" % source)
+		continue
+
+	# if --force option is given, delete the previously existing symlink
+	if os.path.lexists(target) and os.path.islink(target) and options.force == True:
+		os.unlink(target)
+
+	# make a symbolic link!
+	if os.path.lexists(target):
+		print >> stderr, ("%s : already exists" % target) + (options.force and ' (not a symlink, hence --force option ignored)' or '')
+	else:
+		os.symlink(source, target)
+		print >> stderr, ("%s : symlink created from '%s'" % (target, source))
+
