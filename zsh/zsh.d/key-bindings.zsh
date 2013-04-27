0593c92b80b2c8d117cbf57d4a976ed6983f4da7
wookayin <wookayin@gmail.com>
2013-04-27 17:58:33 +0900

    Support <Ctrl-R> history search key bindings
    'key-binding.zsh' defines the bash-compatible behavior
    on <Ctrl-R> (backward history incremental search).
    
    Now zshrc sources all the after-prezto zsh files
    located in the directory 'zsh/zsh.d'.

---
+++ zsh/zsh.d/key-bindings.zsh
@@ -0,0 +1,6 @@
+# Custom key bindings for ZSH
+
+# In default settings, we use 'vi-mode' (bindkey -v)
+
+# <Ctrl-R> starts history incremental search. (bash-compatible)
+bindkey '^r' history-incremental-search-backward
