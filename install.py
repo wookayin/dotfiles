aa7241252a53e0293f454bfb282ec56751ccac34
wookayin <wookayin@gmail.com>
2013-05-04 18:18:08 +0900

    Use spaces for indenting install.py

--- install.py
+++ install.py
@@ -7,16 +7,16 @@
 # (path of target symlink) : (location of source file in the repository)
 
 tasks = {
-	# SHELLS
-	'~/.bashrc' : 'bashrc',
-	'~/.screenrc' : 'screenrc',
+    # SHELLS
+    '~/.bashrc' : 'bashrc',
+    '~/.screenrc' : 'screenrc',
 
-	# VIM
-	'~/.vimrc' : 'vimrc',
-	'~/.vim' : 'vim',
+    # VIM
+    '~/.vimrc' : 'vimrc',
+    '~/.vim' : 'vim',
 
-	# GIT
-	'~/.gitconfig' : 'gitconfig',
+    # GIT
+    '~/.gitconfig' : 'gitconfig',
 
     # ZSH
     '~/.zsh'      : 'zsh',
@@ -27,8 +27,8 @@ tasks = {
     '~/.zshenv'   : 'zsh/zshenv',
     '~/.zshrc'    : 'zsh/zshrc',
 
-	# X
-	'~/.Xmodmap' : 'Xmodmap',
+    # X
+    '~/.Xmodmap' : 'Xmodmap',
 }
 
 ################# END OF FIXME #################
@@ -40,33 +40,33 @@ from sys import stderr
 
 # command line arguments
 def option():
-	parser = OptionParser()
-	parser.add_option("-f", "--force", action="store_true", default=False)
-	(options, args) = parser.parse_args()
-	return options
+    parser = OptionParser()
+    parser.add_option("-f", "--force", action="store_true", default=False)
+    (options, args) = parser.parse_args()
+    return options
 
 # get current directory (absolute path) and options
 current_dir = os.path.abspath(os.path.dirname(__file__))
 options = option()
 
 for target, source in tasks.items():
-	# normalize paths
-	source = os.path.join(current_dir, source)
-	target = os.path.expanduser(target)
+    # normalize paths
+    source = os.path.join(current_dir, source)
+    target = os.path.expanduser(target)
 
-	# if source does not exists...
-	if not os.path.lexists(source):
-		print >> stderr, ("source %s : does not exists" % source)
-		continue
+    # if source does not exists...
+    if not os.path.lexists(source):
+        print >> stderr, ("source %s : does not exists" % source)
+        continue
 
-	# if --force option is given, delete the previously existing symlink
-	if os.path.lexists(target) and os.path.islink(target) and options.force == True:
-		os.unlink(target)
+    # if --force option is given, delete the previously existing symlink
+    if os.path.lexists(target) and os.path.islink(target) and options.force == True:
+        os.unlink(target)
 
-	# make a symbolic link!
-	if os.path.lexists(target):
-		print >> stderr, ("%s : already exists" % target) + (options.force and ' (not a symlink, hence --force option ignored)' or '')
-	else:
-		os.symlink(source, target)
-		print >> stderr, ("%s : symlink created from '%s'" % (target, source))
+    # make a symbolic link!
+    if os.path.lexists(target):
+        print >> stderr, ("%s : already exists" % target) + (options.force and ' (not a symlink, hence --force option ignored)' or '')
+    else:
+        os.symlink(source, target)
+        print >> stderr, ("%s : symlink created from '%s'" % (target, source))
 
