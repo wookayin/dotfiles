fe05374545abcc3c86870655610920876a6e1bef
wookayin <wookayin@gmail.com>
2013-05-18 00:56:22 +0900

    Add alias commands setting for zsh

---
+++ zsh/zsh.d/alias.zsh
@@ -0,0 +1,20 @@
+# Custom Alias commands for ZSH
+
+# Basic
+alias cp='nocorrect cp -iv'
+alias mv='nocorrect mv -iv'
+alias rm='nocorrect rm -iv'
+
+# Screen
+alias scr='screen -rD'
+
+# More Git aliases
+# (overrides przto's git/alias.zsh)
+
+alias gh='git history'
+alias gha='gh --all'
+alias gd='git diff'
+alias gdc='gd --cached'
+alias gs='git status'
+alias gsu='gs -u'
+
