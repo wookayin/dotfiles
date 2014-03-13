58e6c4295e55164d0230844685c1a1531155d420
wookayin <wookayin@gmail.com>
2014-03-13 22:59:41 +0900

    Zsh: Add bash-compatible (emacs style) key bindings
    Emacs-style key bindings such as ^A, ^E are unavailable in ZSH by
    default. Therefore we manually add some commonly used key bindings.

--- zsh/zsh.d/key-bindings.zsh
+++ zsh/zsh.d/key-bindings.zsh
@@ -2,5 +2,21 @@
 
 # In default settings, we use 'vi-mode' (bindkey -v)
 
-# <Ctrl-R> starts history incremental search. (bash-compatible)
-bindkey '^r' history-incremental-search-backward
+# Bash-compatible (emacs style) key bindings
+# ==========================================
+# @see http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html
+# @see http://www.gnu.org/software/bash/manual/html_node/Readline-Interaction.html
+
+bindkey '^A' beginning-of-line
+bindkey '^E' end-of-line
+
+bindkey '^B' backward-char
+bindkey '^F' forward-char
+
+bindkey '^D' delete-char
+bindkey '^H' backward-delete-char
+
+bindkey '^N' down-history
+bindkey '^P' up-history
+
+bindkey '^R' history-incremental-search-backward
