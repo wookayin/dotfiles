9a3d8b88c78c8e51c2c35da7860d8dedd4b99a3a
wookayin <wookayin@gmail.com>
2015-05-10 22:38:05 +0900

    Zsh: Save more history entries in zhistory

---
+++ zsh/zsh.d/envs.zsh
@@ -0,0 +1,8 @@
+# Environment Variables and Shell Options for ZSH
+# (overrides prezto's default settings or zshenv)
+
+
+# Save more history entries
+# @see history/init.zsh
+export HISTSIZE=100000
+export SAVEHIST=100000
