ca7aed8e12db0b4f9473cb4ac6943d6cf47d8e5c
Jongwook Choi <wookayin@gmail.com>
2016-03-28 20:12:45 +0900

    Zsh: Add envvar 'FZF_CTRL_R_OPTS' for fzf
    The previous default configuration in fzf's <Ctrl-R> command
    (for searching through command history) was without the sorted mode.
    To hit the best match to the query, we had to toggle the sort mode
    by pressing another <CTRL-R>, which is pretty unhandy.
    
    A recent version of fzf started to support 'FZF_CTRL_R_OPTS',
    thereby allowing to pass the '--sort' flag to fzf invoked with
    <Ctrl-R> by default, so that we can easily look up the desired command.
    
    References: junegunn/fzf#526

--- zsh/zsh.d/envs.zsh
+++ zsh/zsh.d/envs.zsh
@@ -2,6 +2,10 @@
 # (overrides prezto's default settings or zshenv)
 
 
+# fzf-powered CTRL-R: launch fzf with sort enabled
+# @see https://github.com/junegunn/fzf/issues/526
+export FZF_CTRL_R_OPTS='--sort'
+
 # Save more history entries
 # @see history/init.zsh
 export HISTSIZE=100000
