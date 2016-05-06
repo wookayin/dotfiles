7192093978833d68cc24b1360e06aa9aa84a06ae
Jongwook Choi <wookayin@gmail.com>
2016-05-07 04:25:48 +0900

    Etc: Normalize whitespaces

--- etc/ubuntu-setup.sh
+++ etc/ubuntu-setup.sh
@@ -1,57 +1,56 @@
 #!/bin/bash
-# vim: set noet:
 
 install_essential_packages() {
-	local -a packages; packages=( \
-		build-essential \
-		vim zsh curl \
-		python-software-properties software-properties-common \
-		cmake cmake-data ctags \
-		terminator htop \
-		silversearcher-ag \
-		openssh-server mosh \
-		)
+    local -a packages; packages=( \
+        build-essential \
+        vim zsh curl \
+        python-software-properties software-properties-common \
+        cmake cmake-data ctags \
+        terminator htop \
+        silversearcher-ag \
+        openssh-server mosh \
+        )
 
-	sudo apt-get install -y ${packages[@]}
+    sudo apt-get install -y ${packages[@]}
 }
 
 install_ppa_git() {
-	# https://launchpad.net/~git-core/+archive/ubuntu/ppa
-	sudo add-apt-repository -y ppa:git-core/ppa
-	sudo apt-get update
-	sudo apt-get install -y git-all
+    # https://launchpad.net/~git-core/+archive/ubuntu/ppa
+    sudo add-apt-repository -y ppa:git-core/ppa
+    sudo apt-get update
+    sudo apt-get install -y git-all
 }
 
 install_ppa_tmux() {
-	# https://launchpad.net/~pi-rho/+archive/ubuntu/dev
-	sudo add-apt-repository -y ppa:pi-rho/dev
-	sudo apt-get update
-	sudo apt-get install -y tmux
+    # https://launchpad.net/~pi-rho/+archive/ubuntu/dev
+    sudo add-apt-repository -y ppa:pi-rho/dev
+    sudo apt-get update
+    sudo apt-get install -y tmux
 }
 
 install_ppa_nginx() {
-	# https://launchpad.net/~nginx/+archive/ubuntu/stable
-	sudo add-apt-repository -y ppa:nginx/stable
-	sudo apt-get update
-	sudo apt-get install -y nginx
+    # https://launchpad.net/~nginx/+archive/ubuntu/stable
+    sudo add-apt-repository -y ppa:nginx/stable
+    sudo apt-get update
+    sudo apt-get install -y nginx
 }
 
 install_all() {
-	# TODO dependency management: duplicated 'apt-get update'?
-	install_essential_packages
-	install_ppa_tmux
-	install_ppa_git
-	install_ppa_nginx
+    # TODO dependency management: duplicated 'apt-get update'?
+    install_essential_packages
+    install_ppa_tmux
+    install_ppa_git
+    install_ppa_nginx
 }
 
 
 # entrypoint script
 if [ `uname` != "Linux" ]; then
-	echo "Run on Linux (not on Mac OS X)"; exit 1
+    echo "Run on Linux (not on Mac OS X)"; exit 1
 fi
 if [ -n "$1" ]; then
-	$1
+    $1
 else
-	echo "Usage: $0 [command], where command is one of the following:"
-	declare -F | cut -d" " -f3
+    echo "Usage: $0 [command], where command is one of the following:"
+    declare -F | cut -d" " -f3
 fi
