f7d3bbdf9012ffca146172376736211c293ce4aa
Jongwook Choi <wookayin@gmail.com>
2016-11-11 22:25:37 +0900

    Zsh: Alias 'vi' and 'vim' for neovim
    Neovim is the default vim :-)

--- zsh/zsh.d/alias.zsh
+++ zsh/zsh.d/alias.zsh
@@ -8,12 +8,11 @@ alias rm='nocorrect rm -iv'
 # Screen
 alias scr='screen -rD'
 
-# v: Neovim (if exists) or Vim
+# vim: Defaults to Neovim if exists
 if command -v nvim 2>&1 >/dev/null; then
-    alias v='nvim'
-else
-    alias v='vim'
+    alias vim='nvim'
 fi
+alias vi='vim'
 
 # Tmux ========================================= {{{
 
