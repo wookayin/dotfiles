5446b4ceb95cc44d10c619eb02fe5cedc26402f7
Jongwook Choi <wookayin@gmail.com>
2016-12-24 01:24:31 +0900

    Zsh: Use fasd_cd (z) as the command for fzf's <ALT-C> binding
    By pressing <ALT-C>, one can quickly fzf-navigate to directory
    among the list same as 'z' (fasd_cd -d -l).
    
    An optional preview with the 'tree' command is also used.

--- zsh/zsh.d/envs.zsh
+++ zsh/zsh.d/envs.zsh
@@ -2,9 +2,18 @@
 # (overrides prezto's default settings or zshenv)
 
 
+# fzf {{{
+#https://github.com/junegunn/fzf/wiki/Configuring-shell-key-bindings
+
 # fzf-powered CTRL-R: launch fzf with sort enabled
 # @see https://github.com/junegunn/fzf/issues/526
 export FZF_CTRL_R_OPTS='--sort'
+
+# ALT-C: FASD_CD with preview
+export FZF_ALT_C_COMMAND='fasd_cd -d -l -R'
+export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
+
+# }}}
 
 # Save more history entries
 # @see history/init.zsh
