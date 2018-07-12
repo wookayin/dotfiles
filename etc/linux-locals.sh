2339c15c6a1b43c3d9d24ff7169b252bc6039a49
Jongwook Choi <wookayin@gmail.com>
2018-07-11 20:55:29 -0400

    etc: Update bazel version to 0.15.0

--- etc/linux-locals.sh
+++ etc/linux-locals.sh
@@ -89,7 +89,7 @@ install_tmux() {
 install_bazel() {
     set -e
 
-    BAZEL_VER="0.5.4"
+    BAZEL_VER="0.15.0"
     BAZEL_URL="https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VER}/bazel-${BAZEL_VER}-installer-linux-x86_64.sh"
 
     TMP_BAZEL_DIR="/tmp/$USER/bazel/"
