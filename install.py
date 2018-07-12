wookayin/dotfiles

commit c48f9aed772d32cf510421c7c8c2e72e5534c181

Make install.py compatible with python3
raw_input() has been eliminated in Python 3.

install.py
@@ -182,6 +182,7 @@ import os
 import sys
 import subprocess
 
+from builtins import input
 from signal import signal, SIGPIPE, SIG_DFL
 from optparse import OptionParser
 from sys import stderr
@@ -211,7 +212,7 @@ if submodule_issues:
 
     log("")
     log(YELLOW("Do you want to update submodules? (y/n) "), cr=False)
-    shall_we = (raw_input().lower() == 'y')
+    shall_we = (input().lower() == 'y')
     if shall_we:
         git_submodule_update_cmd = 'git submodule update --init --recursive'
         # git 2.8+ supports parallel submodule fetching

commit cfdf54955e609042327fb69a6ecb661809964d78

Fix python 2/3 compatibility of install.py
This fixes the regression caused by c48f9aed.

install.py
@@ -182,7 +182,10 @@ import os
 import sys
 import subprocess
 
-from builtins import input
+try:
+    from builtins import input   # python3
+except ImportError:
+    input = raw_input     # python2
 from signal import signal, SIGPIPE, SIG_DFL
 from optparse import OptionParser
 from sys import stderr