1903bc32391b801e1bcb658fe286d6e92abd5271
Jongwook Choi <wookayin@gmail.com>
2018-11-08 22:40:57 -0500

    python: Fix ptpython installation path

--- install.py
+++ install.py
@@ -78,7 +78,7 @@ tasks = {
     '~/.pylintrc' : 'python/pylintrc',
     '~/.condarc' : 'python/condarc',
     '~/.config/pycodestyle' : 'python/pycodestyle',
-    '~/.config/ptpython/config.py' : 'python/ptpython.config.py',
+    '~/.ptpython/config.py' : 'python/ptpython.config.py',
 }
 
 
