e8a2cb1253f80c3ad5309dd6f21b8f218cdeeec6
Jongwook Choi <wookayin@gmail.com>
2019-08-20 23:05:39 -0700

    etc: Require jupyterlab 1.0+ to make labextensions work well

--- etc/setup-jupyter.sh
+++ etc/setup-jupyter.sh
@@ -5,12 +5,15 @@
 node -v || (echo "node.js required"; exit 1)
 set -v
 
-pip install 'jupyterlab'
+pip install 'jupyterlab>=1.0'
 pip install 'ipykernel>=5.0'
 pip install 'ipympl'
-pip install 'ipywidgets'
+pip install 'ipywidgets>=7.5.0'
 
 jupyter nbextension enable --py widgetsnbextension
 
 jupyter labextension install @jupyter-widgets/jupyterlab-manager
 jupyter labextension install jupyter-matplotlib
+
+# show installed/enabled extensions
+jupyter labextension list
