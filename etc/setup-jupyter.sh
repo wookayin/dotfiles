feea993924649473451745d5a1d29d8dcc84e389
Jongwook Choi <wookayin@gmail.com>
2019-09-15 12:06:17 -0700

    etc: Require jupyterlab 1.1+

--- etc/setup-jupyter.sh
+++ etc/setup-jupyter.sh
@@ -5,7 +5,7 @@
 node -v || (echo "node.js required"; exit 1)
 set -v
 
-pip install 'jupyterlab>=1.0'
+pip install 'jupyterlab>=1.1'
 pip install 'ipykernel>=5.0'
 pip install 'ipympl'
 pip install 'ipywidgets>=7.5.0'
