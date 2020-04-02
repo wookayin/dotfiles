6709e028b97249e01b81fceec43d6b1041d29711
Jongwook Choi <wookayin@gmail.com>
2020-04-02 16:33:25 -0700

    zsh: Do not override GIT_EDITOR if the variable is already set

--- zsh/zsh.d/envs.zsh
+++ zsh/zsh.d/envs.zsh
@@ -54,7 +54,7 @@ unsetopt AUTO_NAME_DIRS       # Do not auto add variable-stored paths
 unsetopt NOMATCH
 
 # Editor
-if (( $+commands[nvim] )); then
+if (( $+commands[nvim] )) && [[ -z "$GIT_EDITOR" ]] ; then
   export GIT_EDITOR="nvim"
 fi
 
