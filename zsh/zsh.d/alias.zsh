92a1a275a3668a2d4f70d98bcaa13e21f130d604
Jongwook Choi <wookayin@gmail.com>
2020-07-22 04:37:29 -0400

    zsh: Fix issues with old git versions (<2.0) on alias `gha`
    Git < 1.9 does not support `git log --exclude=...` options.
    This breaks the alias `gha` on legacy systems; we fall back to
    `gh --all` if git version is too old.

--- zsh/zsh.d/alias.zsh
+++ zsh/zsh.d/alias.zsh
@@ -1,5 +1,13 @@
 # Custom alias and functions for ZSH
 
+# -------- Utilities ----------
+_version_check() {
+    # _version_check curver targetver: returns true (zero exit code) if $curver >= $targetver
+    curver="$1"; targetver="$2";
+    [ "$targetver" = "$(echo -e "$curver\n$targetver" | sort -V | head -n1)" ]
+}
+# -----------------------------
+
 # Basic
 alias reload!=". ~/.zshrc && echo 'sourced ~/.zshrc' again"
 alias c='command'
@@ -103,9 +111,16 @@ compdef '_hosts' ssh-tmuxa
 # More Git aliases ============================= {{{
 # (overrides prezto's default git/alias.zsh)
 
+GIT_VERSION=$(git --version | awk '{print $3}')
+
 alias gh='git history'
-alias gha='gh --exclude=refs/stash --all'
 alias ghA='gh --all'
+if _version_check $GIT_VERSION "2.0"; then
+  alias gha='gh --exclude=refs/stash --all'
+else
+  alias gha='gh --all'   # git < 1.9 has no --exclude option
+fi
+
 alias gd='git diff --no-prefix'
 alias gdc='gd --cached --no-prefix'
 alias gds='gd --staged --no-prefix'
