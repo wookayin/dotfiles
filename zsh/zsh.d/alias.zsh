9ad357eed9a1d09ada3f98726fa75a80ef8ff438
Jongwook Choi <wookayin@gmail.com>
2020-12-29 01:55:09 -0500

    zsh: Add command `rgfzf`
    ripgrep followed by fzf. Very useful for code searching.

--- zsh/zsh.d/alias.zsh
+++ zsh/zsh.d/alias.zsh
@@ -314,6 +314,21 @@ function gcp-instances-fzf() {
 # }}}
 
 
+# FZF magics ======================================= {{{
+
+rgfzf () {
+    # ripgrep
+    if [ ! "$#" -gt 0 ]; then
+        echo "Usage: rgfzf <query>"
+        return 1
+    fi
+    rg --files-with-matches --no-messages "$1" | \
+        fzf --prompt "$1 > " \
+        --reverse --multi --preview "rg --ignore-case --pretty --context 10 '$1' {}"
+}
+
+# }}}
+
 # Etc ======================================= {{{
 
 alias iterm-tab-color="noglob iterm-tab-color"
