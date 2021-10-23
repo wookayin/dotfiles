594172460db514df3b3e8dc4726b4e052ba59e65
Jongwook Choi <wookayin@gmail.com>
2021-10-22 20:02:48 -0400

    vim: Make LSP diagnostics popup to not appear again once hidden
    The LSP diagnostic popup window will show again, even after it gets
    hidden due to other events (e.g. documentation, vim.lsp.buf.hover).
    This can be annoying because the CursorHold event will be triggered
    many times and thus the diagnostic popup window won't be dismissed.
    
    A simple trick to fix this problem is to show the LSP diagnostics popup
    only once for the current cursor location. The diagnostics popup menu
    will appear again if the cursor has moved to another location.
    
    See also https://www.reddit.com/r/neovim/comments/mqspfo/is_there_any_way_to_hide_the_diagnostic_from
    Ref: #27

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -164,10 +164,21 @@ else  -- neovim 0.5.0
 end
 
 -- Show diagnostics in a pop-up window on hover
+_G.LspDiagnosticsPopupHandler = function()
+  local current_cursor = vim.api.nvim_win_get_cursor(0)
+  local last_popup_cursor = vim.w.lsp_diagnostics_last_cursor or {nil, nil}
+
+  -- Show the popup diagnostics window,
+  -- but only once for the current cursor location (unless moved afterwards).
+  if not (current_cursor[1] == last_popup_cursor[1] and current_cursor[2] == last_popup_cursor[2]) then
+    vim.w.lsp_diagnostics_last_cursor = current_cursor
+    _G.LspDiagnosticsShowPopup()
+  end
+end
 vim.cmd [[
 augroup LSPDiagnosticsOnHover
   autocmd!
-  autocmd CursorHold *   lua _G.LspDiagnosticsShowPopup()
+  autocmd CursorHold *   lua _G.LspDiagnosticsPopupHandler()
 augroup END
 ]]
 
