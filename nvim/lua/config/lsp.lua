7ea37d3e69e37496330e450f7bb76d9464398c11
Jongwook Choi <wookayin@gmail.com>
2022-06-30 00:11:44 -0400

    vim: Prevent sumneko_lua LSP server from auto-formatting.

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -66,6 +66,11 @@ local on_attach = function(client, bufnr)
   --buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
   --buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
   --buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+
+  -- Disable specific LSP capabilities: see nvim-lspconfig#1891
+  if client.name == "sumneko_lua" and client.server_capabilities then
+    client.server_capabilities.documentFormattingProvider = false
+  end
 end
 
 -- Add global keymappings for LSP actions
