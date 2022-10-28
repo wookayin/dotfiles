wookayin/dotfiles

commit ef1481b30d51c2893ddd5a8d484cb013ffd582ce

vim: Do not try to install missing plugins if they are local-only

vim/plugins.vim
@@ -279,7 +279,7 @@ silent unlet g:_nerdtree_lazy_events
 
 
 " Automatically install missing plugins on startup
-let g:plugs_missing_on_startup = filter(values(g:plugs), '!isdirectory(v:val.dir)')
+let g:plugs_missing_on_startup = filter(values(g:plugs), '!isdirectory(v:val.dir) && get(v:val, "uri")')
 if len(g:plugs_missing_on_startup) > 0
   PlugInstall --sync | q
 endif

commit e2fa569c45a48ef6553addae8a24fc298e090e91

vim: Fix vim-plug autoinstall was broken since ef1481b

vim/plugins.vim
@@ -279,7 +279,7 @@ silent unlet g:_nerdtree_lazy_events
 
 
 " Automatically install missing plugins on startup
-let g:plugs_missing_on_startup = filter(values(g:plugs), '!isdirectory(v:val.dir) && get(v:val, "uri")')
+let g:plugs_missing_on_startup = filter(values(g:plugs), '!isdirectory(v:val.dir) && !empty(get(v:val, "uri"))')
 if len(g:plugs_missing_on_startup) > 0
   PlugInstall --sync | q
 endif