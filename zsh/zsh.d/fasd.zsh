ccb391cfcf95ce589bdaf84323da4ea3b061a932
Jongwook Choi <wookayin@gmail.com>
2023-03-23 19:23:40 -0400

    zsh(alias): Fix the 'z' command not accepting the absolute PATH
    The alias `z = fasd_cd -d` should work exactly the same as `cd`
    if the argument represent a valid, existing absolute path.

--- zsh/zsh.d/fasd.zsh
+++ zsh/zsh.d/fasd.zsh
@@ -8,5 +8,12 @@ alias d='fasd -d'        # directory
 alias f='fasd -f'        # file
 alias sd='fasd -sid'     # interactive directory selection
 alias sf='fasd -sif'     # interactive file selection
-alias z='fasd_cd -d'     # cd, same functionality as j in autojump
 alias zz='fasd_cd -d -i' # cd with interactive selection
+
+function z() {
+    # cd, same functionality as j in autojump
+    # need to strip trailing '/' or recognize the existing path as-is
+    # because fasd_cd won't accept absolute path
+    local arg=${@%$'/'}
+    fasd_cd -d "$arg"
+}
