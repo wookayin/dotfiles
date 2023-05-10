2f4c66a6faab0ea93df6a8187e19263fe909d0a7
Jongwook Choi <wookayin@gmail.com>
2023-05-10 02:50:02 -0400

    vim(plugins): Pin nvim-treesitter at v0.7.2 temporarily
    nvim-treesitter >= 0.8 is not compatible with neovim 0.8.x. Until we
    figure out a workaround or neovim 0.8 support is dropped (or until
    I find some needs to use the recent treesitter queries and parsers),
    we can pin nvim-treesitter at an old version that is both compatible
    with neovim 0.8, 0.9, and nightly.

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -5,11 +5,14 @@ local function has(f) return vim.fn.has(f) > 0 end
 
 return {
   Plug 'nvim-treesitter/nvim-treesitter' {
-    build = ':TSUpdateSync',
     version = (function()
-      if not has('nvim-0.8') then return 'v0.7.2' end
-      return '*'  -- use stable release
+      -- Use the old, stable version that is compatible with the minimum supported neovim verison (0.8.x).
+      -- There will be a lot of breaking changes in treesitter; when bumped up, check whether treesitter highlighter works OK.
+      -- Note: nvim-treesitter v0.8.0+ is not compatible with neovim 0.8.x
+      -- see https://github.com/nvim-treesitter/nvim-treesitter/issues/3092
+      return 'v0.7.2'
     end)(),
+    build = ':TSUpdateSync',
     lazy = true,  -- see config/treesitter.lua
   };
 
