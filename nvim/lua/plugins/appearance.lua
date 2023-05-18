355782a6b7d792de712354431d08d6fbb90016ca
Jongwook Choi <wookayin@gmail.com>
2023-05-17 22:04:43 -0400

    vim(highlight): Add colorscheme plugin 'kanagawa'

--- nvim/lua/plugins/appearance.lua
+++ nvim/lua/plugins/appearance.lua
@@ -5,6 +5,7 @@ local PlugConfig = require('utils.plug_utils').PlugConfig
 
 return {
   Plug 'flazz/vim-colorschemes' { lazy = false, priority = 1000 };
+  Plug 'rebelot/kanagawa.nvim' { lazy = false, priority = 1000 };
 
   -- statusline
   Plug 'nvim-lualine/lualine.nvim' {
