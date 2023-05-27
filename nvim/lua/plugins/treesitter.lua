9cbfe47eeb9a1596881cc38091a61f2a43a635b9
Jongwook Choi <wookayin@gmail.com>
2023-05-26 22:46:31 -0400

    vim(treesitter): Use master version of nvim-treesitter for neovim nightly
    nvim-treesitter v0.9.0 is not compatible with neovim 0.10+ (nightly),
    so a devel version (master branch) is needed for neovim nightly.

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,15 +3,23 @@
 local Plug = require('utils.plug_utils').Plug
 local function has(f) return vim.fn.has(f) > 0 end
 
+local treesitter_version
+-- Use the old, stable version that is compatible with the minimum supported neovim veriso>
+-- There are often a lot of breaking changes in treesitter;
+-- when versions bumped up, check whether treesitter highlighter works OK.
+-- When `query: invalid node type` error happens, run :TSUpdate or :TSInstall! [lang] manu>
+-- see https://github.com/nvim-treesitter/nvim-treesitter/issues/3092
+if not has('nvim-0.10') then
+  treesitter_version = '*'  -- Use the latest stable version (0.9.0), works for neovim 0.8+
+else
+  treesitter_version = nil  -- Use the 'master' branch.
+end
+
 return {
   Plug 'nvim-treesitter/nvim-treesitter' {
-    version = (function()
-      -- Use the old, stable version that is compatible with the minimum supported neovim verison (0.8.x).
-      -- There will be a lot of breaking changes in treesitter; when bumped up, check whether treesitter highlighter works OK.
-      -- Note: nvim-treesitter v0.8.0+ is not compatible with neovim 0.8.x
-      -- see https://github.com/nvim-treesitter/nvim-treesitter/issues/3092
-      return 'v0.7.2'
-    end)(),
+    name = 'nvim-treesitter',
+    version = treesitter_version,
+    branch = (treesitter_version == nil) and 'master' or nil,
     build = function(_)
       -- :TSUpdateSync (blocks UI)
       (require('nvim-treesitter.install').update { with_sync = true })()
