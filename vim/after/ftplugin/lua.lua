27362a954670d76a67cb3f45b2179e71341a3fcf
Jongwook Choi <wookayin@gmail.com>
2023-07-31 00:44:21 -0400

    vim(lua): <F5> or :Build should execute non-config lua files as well
    As well as config files (nvim/config/**/*.lua), any lua file outside
    the neovim config path can be also executed through :Build.

--- vim/after/ftplugin/lua.lua
+++ vim/after/ftplugin/lua.lua
@@ -6,8 +6,8 @@ setlocal.sts = 2
 setlocal.sw = 2
 
 
--- <F5> or :Make ==> source it, if a (neo)vim config
-if string.find(vim.fn.expand("%:p"), "nvim/lua/config/") then
+-- <F5> or :Build => source (execute) the lua file
+if vim.fn.filereadable('Makefile') == 0 then
   vim.api.nvim_buf_create_user_command(0, 'Build', function(opts)
     vim.cmd [[
       update
@@ -15,7 +15,7 @@ if string.find(vim.fn.expand("%:p"), "nvim/lua/config/") then
     ]]
     vim.notify("Sourced " .. vim.fn.bufname())
   end,
-  { desc = 'Build: source lua config script.', nargs = 0 })
+  { desc = 'Build: source as a lua script.', nargs = 0 })
 end
 
 -- Auto-reload hammerspoon config when applicable.
