1d9d62e86393cc06e729b5d59c77b213bfab0a9c
Jongwook Choi <wookayin@gmail.com>
2023-08-14 19:57:53 -0400

    vim(treesitter): Auto-install bibtex TS parsers

---
+++ vim/after/ftplugin/bib.lua
@@ -0,0 +1,11 @@
+-- ftplugin for bibtex files
+-- Loads and works only on neovim 0.7.0 or higher
+
+
+-- Ensure bibtex TS parsers are installed when opening .bib files for the first time
+if pcall(require, 'nvim-treesitter.parsers') then
+  local parsers = require 'nvim-treesitter.parsers'
+  if not parsers.get_parser(0, 'bibtex') then
+    require("nvim-treesitter.install").ensure_installed({ 'bibtex' })
+  end
+end
