3d6969937c6f7b28276b0cc72f33a42c18e267b1
Jongwook Choi <wookayin@gmail.com>
2023-09-14 20:11:11 -0400

    vim(fzf): Replace fzf.vim commands with fzf-lua
    Replaced (from fzf.vim):
    
    - `:Files [dir]`
    - `:GitFiles [dir]` (note: no longer accepts git flags)
    - `:GFiles [path]` => ditto
    - `:GFiles?` => renamed to `:GStatus`
    - `:Buffers [init_query]`
    - `:Lines [init_query]`
    - `:BLines [init_query]`
    - `:Colors [init_query]`
    - `:Rg`, `:RG` => Unified with `:Grep` (see note below)
    - `:Tags [init_query]`
    - `:BTags [init_query]`
    - `:Jumps`
    - `:Marks`
    - `:Help [init_query]` (= helptags)
    - `:Commits`
    - `:BCommits`
    - `:Filetypes`  (almost no difference)
    - `:History`  (= oldfiles)
    
    The legacy fzf.vim commands are not removed yet, but are prefixed with
    `Fzf`, e.g., `:Files` => `:FzfFiles`.
    
    Note on rigrep (`:Grep`), a bit of breaking changes:
    
    - The distinction `:Rg` v.s. `:RG` (or `RgFzf`) was quite confusing.
      Those grep-like commands are unified into the single `:Grep`.
    - `:Grep [query]` first greps with `query`, and filter via fzf
    - `:Grep! [query]` does "live-grep", the grep string is what we input
    - We can *interactively* and easily toggle between these two by CTRL-g.
    - `:Rg` and `:RG` are simply aliased and redirected to `:Grep`,
      but later `:Rg` might become more like a "raw" ripgrep command.
    
    Removed:
    
    - `:Maps`     => Don't use fzf-lua, not better than fzf.vim or telescope
    - `:Commands` => Don't use fzf-lua, not better than fzf.vim
    - `:Snippets` => TODO, fzf-lua doesn't have it
    - `:Windows`  => TODO, fzf-lua doesn't jave it
    - `:Locate`   => removed, not used
    - `:Ag`       => removed, not used

--- nvim/lua/config/fzf.lua
+++ nvim/lua/config/fzf.lua
@@ -63,10 +63,87 @@ function M.setup()
   local command_alias = vim.fn.CommandAlias
   command_alias("FL", "FzfLua")
 
-  -- TODO: Migrate commands provided by $VIMPLUG/fzf.vim/plugin/fzf.vim:53
-  -- Files, GitFiles, GFiles, Buffers, Lines, BLines, Colors, Locate,
-  -- Ag, Rg, RG, Tags, BTags, Snippets, Commands, Jumps, Marks, Helptags,
-  -- Windows, Commits, BCommits, Maps, Filetypes, History
+  local command = function(name, opts, rhs)
+    vim.api.nvim_create_user_command(name, rhs, opts)
+    return {
+      alias = function(self, lhs, ...) command_alias(lhs, name, ...); return self; end
+    }
+  end
+  local fzf = require("fzf-lua")
+  local empty_then_nil = function(x) return x ~= "" and x or nil; end
+  -- wrap a fzf-lua provider to a neovim user_command function
+  -- with the (optional) argument set as the default fzf query.
+  local bind_query = function(fzf_provider)
+    return function(e)
+      fzf_provider({ query = empty_then_nil(e.args) })
+    end
+  end
+
+  -- Finder (fd, rg, grep, etc.)
+  -- Note: fzf-lua grep uses rg internally
+  command("Files", { nargs = "?", complete = "dir", desc = "FzfLua files" }, function(e)
+    fzf.files({ cwd = empty_then_nil(e.args) })
+  end)
+  command("History", {}, "FzfLua oldfiles"):alias("H")
+  command("Grep", { nargs = "?", bang = true, desc = "FzfLua grep" }, function(e)
+    local args = e.args:gsub('\n', '')
+    if not e.bang then
+      fzf.grep({ search = args })
+    else
+      fzf.live_grep({ search = args })
+    end
+  end)
+  command_alias("Rg", "Grep")
+  command("LiveGrep", { nargs = "?", desc = "FzfLua live_grep" }, function(e)
+    fzf.live_grep({ search = e.args })
+  end)
+  if vim.fn.executable("rg") == 0 then
+    local msg = "rg (ripgrep) not found. Try `dotfiles install rg`."
+    vim.notify(msg, vim.log.levels.WARN, { title = "config.fzf" })
+  end
+  command_alias("RG", "LiveGrep")
+  -- keymaps (grep): CTRL-g, <leader>rg
+  vim.keymap.set('n', '<C-g>', '<cmd>LiveGrep<CR>')
+  vim.keymap.set('n', '<leader>rg', function()
+    fzf.grep({ search = vim.fn.expand("<cword>") })
+  end, { desc = ':Grep with <cword>' })
+  vim.keymap.set('x', '<C-g>', '<leader>rg', { remap = true, silent = true,
+    desc = ':Grep with visual selection' } )
+  vim.keymap.set('x', '<leader>rg', [["gy:Grep <C-R>g<CR>]], { silent = true,
+    desc = ':Grep with visual selection' } )
+
+
+  -- Git providers
+  command("Commits", {}, "FzfLua git_commits")  -- for the CWD. TODO: Support file arg
+  command("BCommits", {}, "FzfLua git_bcommits")  -- for the buffer.
+  command("GitStatus", { nargs = 0 }, "FzfLua git_status"
+    ):alias("GStatus"):alias("GS")
+  command("GitFiles", { nargs = "?", bang = true, complete = "dir", desc = "FzfLua git_files" }, function(e)
+    if e.args == "?" then  -- GFiles?
+      return vim.cmd [[ GitStatus ]]
+    end
+    local opts = { cwd = empty_then_nil(e.args) }
+    if e.bang then  -- GFiles!: include untracked files as well
+      opts.cmd = "git ls-files --exclude-standard --cached --modified --others --deduplicate"
+    end
+    -- Note: Unlike junegunn's GitFiles, it no longer accepts CLI flag. Use Lua API instead
+    fzf.git_files(opts)
+  end):alias("GFiles", { register_cmd = true }):alias("GF")
+  command("Tags", { nargs = "?" }, bind_query(fzf.tags))
+
+  -- neovim providers
+  -- (some commands are provided by telescope because it's often better with telescope)
+  --    :Commands, :Maps, :Highlights
+  command("Buffers", { nargs = "?", complete = "buffer" }, bind_query(fzf.buffers)):alias("B")
+  vim.keymap.set('n', '<leader>B', '<Cmd>Buffers<CR>')
+  command("Colors", { nargs = "?", complete = "color" }, bind_query(fzf.colorschemes))
+  command("Help", { nargs = "?", complete = "help" }, bind_query(fzf.help_tags))
+  command("Lines", { nargs = "?" }, bind_query(fzf.lines))
+  command("BLines", { nargs = "?" }, bind_query(fzf.blines))
+  command("BTags", { nargs= "?" }, bind_query(fzf.btags))
+  command("Marks", {}, "FzfLua marks")
+  command("Jumps", {}, "FzfLua jumps")
+  command("Filetypes", {}, "FzfLua filetypes")
 
   ---[[ Keymaps ]]
   -- TODO: Add insert mode keymaps <plug>(fzf-complete-*)
