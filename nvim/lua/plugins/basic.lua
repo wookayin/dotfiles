45cb992e9a6712f2fe2dfc24ae24f831acd3d0fd
Jongwook Choi <wookayin@gmail.com>
2023-10-11 00:12:19 -0400

    vim(keymap): Add keymaps for lazy-loaded plugin helpful.vim

--- nvim/lua/plugins/basic.lua
+++ nvim/lua/plugins/basic.lua
@@ -2,6 +2,7 @@
 
 local Plug = require('utils.plug_utils').Plug
 
+---@diagnostic disable: missing-fields
 return {
   Plug 'nvim-lua/plenary.nvim' { priority = 10000 };
   Plug 'rcarriga/nvim-notify' { config = require 'config.ui'.setup_notify };
@@ -12,6 +13,10 @@ return {
     init = function()
       pcall(vim.fn.CommandAlias, "HV", "HelpfulVersion")
     end,
+    keys = {
+      { '<leader>hv', mode = 'n' },
+      { '<leader>hv', mode = 'v' },
+    },
     config = function()
       vim.keymap.set({'n', 'v'}, '<leader>hv', '<Cmd>echon ""<CR><Cmd>call helpful#cursor_word()<CR>')
     end,
