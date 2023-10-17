61c32fa889b610f338d7b726b70646f2efb4ac83
Jongwook Choi <wookayin@gmail.com>
2023-10-16 23:36:15 -0400

    vim(help): Show :HelpfulVersion information as the cursor is moved

--- nvim/after/ftplugin/help.lua
+++ nvim/after/ftplugin/help.lua
@@ -9,3 +9,14 @@ local nbufmap = function(lhs, rhs) vim.keymap.set('n', lhs, rhs, { buffer = true
 nbufmap('gd', '<C-]>')
 nbufmap('<CR>', '<C-]>')
 nbufmap('<C-[>', '<C-o>')
+
+--- [[ helpful.vim ]]
+-- Automatically show :HelpfulVersion information as the cursor is moved
+-- and as soon as we enter the buffer for the first time
+-- (Note: do not use b:helpful = 1 because we want to control autocmd on our own)
+vim.cmd [[
+  augroup helpful_auto
+    autocmd! * <buffer>
+    autocmd CursorMoved <buffer> call helpful#cursor_word()
+  augroup END
+]]
