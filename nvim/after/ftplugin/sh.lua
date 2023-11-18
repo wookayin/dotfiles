46db2363fb30fac247512b66f77bd01a4550269f
Jongwook Choi <wookayin@gmail.com>
2023-11-18 10:13:49 -0500

    vim(zsh): Add ftplugin for zsh and bash, set indent size

---
+++ nvim/after/ftplugin/sh.lua
@@ -0,0 +1,5 @@
+-- ftplugin/sh.lua (bash)
+
+vim.opt_local.ts = 2
+vim.opt_local.sts = 2
+vim.opt_local.sw = 2
---
+++ nvim/after/ftplugin/zsh.lua
@@ -0,0 +1,5 @@
+-- ftplugin/zsh.lua
+
+vim.opt_local.ts = 2
+vim.opt_local.sts = 2
+vim.opt_local.sw = 2
