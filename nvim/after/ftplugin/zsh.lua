b575a6eaf4479e89d0411a69f7e3179bce1a60c3
Jongwook Choi <wookayin@gmail.com>
2023-11-20 01:38:09 -0500

    vim(zsh): Makeprg support for zsh unit tests (zunit)

--- nvim/after/ftplugin/zsh.lua
+++ nvim/after/ftplugin/zsh.lua
@@ -8,3 +8,8 @@
 vim.opt_local.ts = 2
 vim.opt_local.sts = 2
 vim.opt_local.sw = 2
+
+-- Make support for easily running unit tests
+if vim.endswith(vim.fn.bufname() or "", "_test.zsh") then
+  vim.opt_local.makeprg = 'zunit --verbose %'
+end
