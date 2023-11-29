91b77f0381748387b70420346ed1e5338a5ab0d1
Jongwook Choi <wookayin@gmail.com>
2023-11-29 01:33:31 -0500

    vim(treesitter): Add a insert-aware version of get_node_at_cursor()
    The `nvim-treesitter.ts_utils.get_node_at_cursor()` utility is not aware
    of the "insert mode", so there is an off-by-one error when this is used
    in the insert mode. See the luadoc for more details.

--- nvim/lua/utils/ts_utils.lua
+++ nvim/lua/utils/ts_utils.lua
@@ -3,24 +3,59 @@
 
 local M = {}
 
+--- Get the treesitter node (the most ancestor) that contains
+--- the current cursor location in the range.
+---
+--- The difference to `nvim-treesitter.ts_utils.get_node_at_cursor()`:
+---
+--- 1. This is aware of the "insert mode" to have a better offset on cursor_range. For example:
+---
+---    1234567 8
+---    "foobar|"
+---    ^^     ^^
+---    ││     ││
+---    ││     │└─ string
+---    ││     └─ cursor (insert mode)
+---    │└─ string_content
+---    └─ string
+---
+---    In the insert mode, the cursor location (1-indexed) will read col = 8, so the
+---    original get_node_at_cursor() implementation will return the `string` node at col = 8.
+---    But in the insert mode, we would want to get the `string_content` node at col = 7.
+---
+--- 2. The return value is never nil. When parser is not available, it will raise errors.
+---
+---    Use vim.F.npcall() to make error-safe!
+---
+--- @param winnr? integer window number, 0 (the current window) by default
+--- @param ignore_injected_langs? boolean
+--- @return TSNode
+function M.get_node_at_cursor(winnr, ignore_injected_langs)
+  winnr = winnr or 0
+  local cursor = vim.api.nvim_win_get_cursor(winnr)  -- line: 1-indexed, col: 0-indexed
+  local insert_offset = ((winnr == 0 or winnr == vim.api.nvim_get_current_win()) and vim.fn.mode() == 'i') and 1 or 0
 
----@return TSNode?
-function M.get_node_at_cursor()
-  local cursor = vim.api.nvim_win_get_cursor(0)
-  local cursor_range = { cursor[1] - 1, cursor[2] - 1 } -- note the new '-1' here
-  local buf = vim.api.nvim_get_current_buf()
-  local parser = vim.F.npcall(vim.treesitter.get_parser, buf)
-  if not parser then
-    return nil
+  -- Treesitter range: row, col are both 0-indexed
+  local cursor_range = { cursor[1] - 1, cursor[2] - insert_offset }
+  local buf = vim.api.nvim_win_get_buf(winnr)
+
+  local root_lang_tree = vim.treesitter.get_parser(buf) ---@type LanguageTree
+  local root ---@type TSNode|nil
+  if ignore_injected_langs then
+    for _, tree in pairs(root_lang_tree:trees()) do
+      local tree_root = tree:root()
+      if tree_root and vim.treesitter.is_in_node_range(tree_root, cursor_range[1], cursor_range[2]) then
+        root = tree_root
+        break
+      end
+    end
+  else
+    root = require("nvim-treesitter.ts_utils").get_root_for_position(cursor_range[1], cursor_range[2], root_lang_tree)
   end
-  local root_tree = parser:parse()[1]
-  local root = root_tree and root_tree:root()
 
-  if not root then
-    return nil
-  end
-
-  return root:named_descendant_for_range(cursor_range[1], cursor_range[2], cursor_range[1], cursor_range[2])
+  assert(root)
+  local node = root:named_descendant_for_range(cursor_range[1], cursor_range[2], cursor_range[1], cursor_range[2])
+  return assert(node)
 end
 
 return M
