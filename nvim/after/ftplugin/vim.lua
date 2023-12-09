e79ac773131e79eab06e4e041c69f7a3e9ce931d
Jongwook Choi <wookayin@gmail.com>
2023-12-08 21:37:51 -0500

    vim(vim): Use treesitter highlight for all neovim versions >= 0.8
    vimscript treesitter parsers seem to be workign fine in nvim 0.8.x.

--- nvim/after/ftplugin/vim.lua
+++ nvim/after/ftplugin/vim.lua
@@ -2,15 +2,9 @@
 
 vim.cmd [[ setlocal ts=2 sts=2 sw=2 ]]
 
--- Workaround for neovim#20456: vim syntax for lua files are broken in neovim 0.8+
-if pcall(require, 'nvim-treesitter') and vim.fn.has('nvim-0.9') > 0 then
-  -- Disable the erroneous $VIMRUNTIME/syntax/lua.vim from loading
-  vim.b.ts_highlight = 1
-
-  -- Use treesitter highlight for vimscripts. (nvim 0.9+)
-  -- Excludes neovim 0.8.x because TS throws an error when parsers are not there yet
-  require('config.treesitter').setup_highlight('vim')
-end
+-- neovim#20456: vim syntax for lua files are broken in neovim 0.8+
+-- Use treesitter highlight for vimscripts.
+require('config.treesitter').setup_highlight('vim')
 
 --- :SourceThis
 --- keys: <F5>, <leader>so
