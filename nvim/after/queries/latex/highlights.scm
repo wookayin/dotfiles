4b90c2a29fa1bf015847c741436be5f090166b11
Jongwook Choi <wookayin@gmail.com>
2023-12-14 01:25:59 -0500

    vim(tex): Highlight underlined text with treesitter

--- nvim/after/queries/latex/highlights.scm
+++ nvim/after/queries/latex/highlights.scm
@@ -2,3 +2,11 @@
 
 ; highlight \iffalse .. \fi comments differently
 ((block_comment) @comment.special)
+
+
+;; Formatting (extended)
+
+((generic_command
+  command: (command_name) @_name
+  arg: (curly_group (_) @text.underline))
+ (#any-of? @_name "\\underline" "\\uline"))
