64884eadfa6414063b996823bc24fe515bb73ec6
Jongwook Choi <wookayin@gmail.com>
2024-01-07 17:14:00 -0500

    vim(completion): Refactoring on nvim-cmp config
    - cmp_helper => use module namespace instead (M)
    - format function is too long, separate out as M.format

--- nvim/lua/config/completion.lua
+++ nvim/lua/config/completion.lua
@@ -25,8 +25,6 @@ local truncate = function(text, max_width)
 end
 
 
-local cmp_helper = {}
-
 M.setup_cmp = function()
   local cmp = require('cmp')
   local SelectBehavior = require('cmp.types.cmp').SelectBehavior
@@ -35,12 +33,15 @@ M.setup_cmp = function()
   vim.o.completeopt = "menu,menuone,noselect"
 
   -- See $VIMPLUG/nvim-cmp/lua/cmp/config/default.lua
+  ---@see cmp.ConfigSchema
   local cmp_config = {
+    ---@see cmp.SnippetConfig
     snippet = {
       expand = function(args)
         vim.fn["UltiSnips#Anon"](args.body)
       end,
     },
+    ---@see cmp.WindowConfig
     window = {
       documentation = {
         border = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' },
@@ -56,6 +57,7 @@ M.setup_cmp = function()
         winhighlight = 'Normal:CmpPmenu,FloatBorder:CmpPmenuBorder,CursorLine:PmenuSel,Search:None',
       }
     },
+    ---@type table<string, cmp.Mapping>
     mapping = {
       -- See $VIMPLUG/nvim-cmp/lua/cmp/config/mapping.lua
       ['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -82,93 +84,13 @@ M.setup_cmp = function()
         end,
       },
     },
+    ---@see cmp.FormattingConfig
     formatting = {
-      ---@param entry cmp.Entry
-      ---@param vim_item vim.CompletedItem
-      format = function(entry, vim_item)
-        -- Truncate the item if it is too long
-        vim_item.abbr = truncate(vim_item.abbr, 80)
-        -- fancy icons and a name of kind
-        pcall(function()  -- protect the call against potential API breakage (lspkind GH-45).
-          local lspkind = require("lspkind")
-          vim_item.kind_symbol = (lspkind.symbolic or lspkind.get_symbol)(vim_item.kind)
-          vim_item.kind = " " .. vim_item.kind_symbol .. " " .. vim_item.kind
-        end)
-
-        -- The 'menu' section: source, detail information (lsp, snippet), etc.
-        -- set a name for each source (see the sources section below)
-        vim_item.menu = ({
-          buffer        = "Buffer",
-          nvim_lsp      = "LSP",
-          ultisnips     = "",
-          nvim_lua      = "Lua",
-          latex_symbols = "Latex",
-        })[entry.source.name] or string.format("%s", entry.source.name)
-
-        -- highlight groups for item.menu
-        vim_item.menu_hl_group = ({
-          buffer = "CmpItemMenuBuffer",
-          nvim_lsp = "CmpItemMenuLSP",
-          path = "CmpItemMenuPath",
-          ultisnips = "CmpItemMenuSnippet",
-        })[entry.source.name]  -- default is CmpItemMenu
-
-        -- detail information (optional)
-        local cmp_item = entry:get_completion_item()  --- @type lsp.CompletionItem
-
-        if entry.source.name == 'nvim_lsp' then
-          -- Display which LSP servers this item came from.
-          local lspserver_name = nil
-          pcall(function()
-            lspserver_name = entry.source.source.client.name
-            vim_item.menu = lspserver_name
-          end)
-
-          -- Some language servers provide details, e.g. type information.
-          -- The details info hide the name of lsp server, but mostly we'll have one LSP
-          -- per filetype, and we use special highlights so it's OK to hide it..
-          local detail_txt = (function()
-            if not cmp_item.detail then return nil end
-
-            if cmp_item.detail == "Auto-import" then
-              local label = (cmp_item.labelDetails or {}).description
-              if not label or label == "" then return nil end
-              local logo = ({
-                pyright = ""
-              })[lspserver_name] or "󰋺"
-              return logo .. " " .. truncate(label, 20)
-            else
-              return truncate(cmp_item.detail, 50)
-            end
-          end)()
-          if detail_txt then
-            vim_item.menu = detail_txt
-            vim_item.menu_hl_group = 'CmpItemMenuDetail'
-          end
-
-        elseif entry.source.name == 'zsh' then
-          -- cmp-zsh: Display documentation for cmdline flag ('' denotes zsh)
-          ---@diagnostic disable-next-line: undefined-field
-          local detail = tostring(cmp_item.documentation)
-          if detail then
-            vim_item.menu = detail
-            vim_item.menu_hl_group = 'CmpItemMenuZsh'
-            vim_item.kind = '  ' .. 'zsh'
-          end
-
-        elseif entry.source.name == 'ultisnips' then
-          ---@diagnostic disable-next-line: undefined-field
-          local description = (cmp_item.snippet or {}).description
-          if description then
-            vim_item.menu = truncate(description, 40)
-          end
-        end
-
-        -- Add a little bit more padding
-        vim_item.menu = " " .. vim_item.menu
-        return vim_item
-      end
+      format = function(...)
+        return M.format(...)
+      end,
     },
+    ---@type cmp.SourceConfig[]
     sources = {
       -- Note: make sure you have proper plugins specified in plugins.vim
       -- https://github.com/topics/nvim-cmp
@@ -178,14 +100,15 @@ M.setup_cmp = function()
       { name = 'omni', priority = 20 },
       { name = 'buffer', priority = 10 },
     },
+    ---@see cmp.SortingConfig
     sorting = {
       -- see $VIMPLUG/nvim-cmp/lua/cmp/config/compare.lua
       comparators = {
         cmp.config.compare.offset,
         cmp.config.compare.exact,
         cmp.config.compare.score,
-        function(...) return cmp_helper.compare.prioritize_argument(...) end,
-        function(...) return cmp_helper.compare.deprioritize_underscore(...) end,
+        function(...) return M.comparators.prioritize_argument(...) end,
+        function(...) return M.comparators.deprioritize_underscore(...) end,
         cmp.config.compare.recently_used,
         cmp.config.compare.kind,
         cmp.config.compare.sort_text,
@@ -212,7 +135,7 @@ M.setup_cmp = function()
   })
 
   -- Highlights
-  require('utils.rc_utils').RegisterHighlights(cmp_helper.apply_highlight)
+  require('utils.rc_utils').RegisterHighlights(M.apply_highlight)
 
   do  -- set pumheight: limit popup menu height
     function _reset_pumheight()
@@ -227,8 +150,95 @@ M.setup_cmp = function()
   end
 end
 
+
+---@param entry cmp.Entry
+---@param vim_item vim.CompletedItem
+function M.format(entry, vim_item)
+  -- Truncate the item if it is too long
+  vim_item.abbr = truncate(vim_item.abbr, 80)
+  -- fancy icons and a name of kind
+  pcall(function()  -- protect the call against potential API breakage (lspkind GH-45).
+    local lspkind = require("lspkind")
+    vim_item.kind_symbol = (lspkind.symbolic or lspkind.get_symbol)(vim_item.kind)
+    vim_item.kind = " " .. vim_item.kind_symbol .. " " .. vim_item.kind
+  end)
+
+  -- The 'menu' section: source, detail information (lsp, snippet), etc.
+  -- set a name for each source (see the sources section below)
+  vim_item.menu = ({
+    buffer        = "Buffer",
+    nvim_lsp      = "LSP",
+    ultisnips     = "",
+    nvim_lua      = "Lua",
+    latex_symbols = "Latex",
+  })[entry.source.name] or string.format("%s", entry.source.name)
+
+  -- highlight groups for item.menu
+  vim_item.menu_hl_group = ({
+    buffer = "CmpItemMenuBuffer",
+    nvim_lsp = "CmpItemMenuLSP",
+    path = "CmpItemMenuPath",
+    ultisnips = "CmpItemMenuSnippet",
+  })[entry.source.name]  -- default is CmpItemMenu
+
+  -- detail information (optional)
+  local cmp_item = entry:get_completion_item()  --- @type lsp.CompletionItem
+
+  if entry.source.name == 'nvim_lsp' then
+    -- Display which LSP servers this item came from.
+    local lspserver_name = nil
+    pcall(function()
+      lspserver_name = entry.source.source.client.name
+      vim_item.menu = lspserver_name
+    end)
+
+    -- Some language servers provide details, e.g. type information.
+    -- The details info hide the name of lsp server, but mostly we'll have one LSP
+    -- per filetype, and we use special highlights so it's OK to hide it..
+    local detail_txt = (function()
+      if not cmp_item.detail then return nil end
+
+      if cmp_item.detail == "Auto-import" then
+        local label = (cmp_item.labelDetails or {}).description
+        if not label or label == "" then return nil end
+        local logo = ({
+          pyright = ""
+        })[lspserver_name] or "󰋺"
+        return logo .. " " .. truncate(label, 20)
+      else
+        return truncate(cmp_item.detail, 50)
+      end
+    end)()
+    if detail_txt then
+      vim_item.menu = detail_txt
+      vim_item.menu_hl_group = 'CmpItemMenuDetail'
+    end
+
+  elseif entry.source.name == 'zsh' then
+    -- cmp-zsh: Display documentation for cmdline flag ('' denotes zsh)
+    ---@diagnostic disable-next-line: undefined-field
+    local detail = tostring(cmp_item.documentation)
+    if detail then
+      vim_item.menu = detail
+      vim_item.menu_hl_group = 'CmpItemMenuZsh'
+      vim_item.kind = '  ' .. 'zsh'
+    end
+
+  elseif entry.source.name == 'ultisnips' then
+    ---@diagnostic disable-next-line: undefined-field
+    local description = (cmp_item.snippet or {}).description
+    if description then
+      vim_item.menu = truncate(description, 40)
+    end
+  end
+
+  -- Add a little bit more padding
+  vim_item.menu = " " .. vim_item.menu
+  return vim_item
+end
+
 -- Custom sorting/ranking for completion items.
-cmp_helper.compare = {
+M.comparators = {
   ---Deprioritize items starting with underscores (private or protected)
   ---@type fun(lhs: cmp.Entry, rhs: cmp.Entry): boolean|nil
   deprioritize_underscore = function(lhs, rhs)
@@ -247,7 +257,7 @@ cmp_helper.compare = {
 }
 
 -- Highlights with bordered completion window (GH-224, GH-472)
-function cmp_helper.apply_highlight()
+function M.apply_highlight()
   vim.cmd [[
     " Dark background, and white-ish foreground
     highlight! CmpPmenu         guibg=#242a30
