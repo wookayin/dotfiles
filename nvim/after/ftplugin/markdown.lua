9500637811b4363d4f103e4427691a14d1c1439a
Jongwook Choi <wookayin@gmail.com>
2024-01-31 21:55:27 -0500

    vim(markdown): Auto-open a brower locally on `:Grip`
    - Use built-in :terminal, not AsyncRun for better control
    - Automatically open a browser if nvim is not running remotely

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -19,9 +19,27 @@ require("config.treesitter").setup_highlight("markdown")
 
 -- GFM markdown preview using grip
 -- (pip install grip)
-vim.cmd [[
-  command! -buffer   Grip AsyncRun grip "%" 0.0.0.0
-]]
+vim.api.nvim_buf_create_user_command(0, 'Grip', function(opts)
+  local win = vim.api.nvim_get_current_win()
+  local exitcode = nil
+  vim.cmd [[ botright 7new ]]
+  vim.cmd [[ setlocal winfixheight ]]
+  vim.fn.termopen({ "grip", vim.fn.expand('%'), "0.0.0.0" }, {
+    on_exit = function(job_id, data, event)
+      exitcode = data
+    end,
+  })
+  vim.api.nvim_set_current_win(win)
+  vim.cmd.stopinsert()  -- workaround for autocmd+terminal bug
+
+  if vim.ui.open and os.getenv('SSH_CONNECTION') == nil then
+    vim.defer_fn(function()
+      if exitcode == nil or exitcode == 0 then
+        vim.ui.open('http://localhost:6419/')
+      end
+    end, 200)
+  end
+end, { nargs = 0 })
 
 -- Markdown headings-
 vim.cmd [[
