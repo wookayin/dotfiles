01850c2053fc9d709fee99eb7e8464317da727a0
Jongwook Choi <wookayin@gmail.com>
2024-05-19 00:14:40 -0400

    vim(lsp): Switch from null-ls.nvim to none-ls.nvim
    null-ls is archived, so we switch to a community fork: none-ls.nvim.
    Existing configs for null-ls still work well and seamlessly.

--- nvim/lua/plugins/ide.lua
+++ nvim/lua/plugins/ide.lua
@@ -39,7 +39,7 @@ return {
   Plug 'folke/neodev.nvim' { event = LspSetup };
   Plug 'ray-x/lsp_signature.nvim' { event = LspSetup };
   Plug 'WhoIsSethDaniel/toggle-lsp-diagnostics.nvim' { lazy = true };
-  Plug 'jose-elias-alvarez/null-ls.nvim' { event = LspSetup, config = require('config.lsp').setup_null_ls };
+  Plug 'nvimtools/none-ls.nvim' { event = LspSetup, config = require('config.lsp').setup_null_ls };
   Plug 'nvim-lua/lsp-status.nvim' { event = LspSetup, config = require('config.lsp').setup_lsp_status };
   Plug 'j-hui/fidget.nvim' { branch = 'legacy', event = LspSetup, config = require('config.lsp').setup_fidget };
   Plug 'folke/trouble.nvim' { event = LspSetup, config = require('config.lsp').setup_trouble };
