23d9a4900d40df147766be37d1a55c8d832330ec
Hyungsuk Yoon <youhanmir@gmail.com>
2024-06-20 13:38:08 -0700

    Update folds.scm

--- nvim/after/queries/python/folds.scm
+++ nvim/after/queries/python/folds.scm
@@ -14,10 +14,10 @@
   . [(import_statement) (import_from_statement) (future_import_statement) (comment)]*
   . [(import_statement) (import_from_statement) (future_import_statement)]+ @_end
   ; ... until the first non-import node.
-  . (_)  @_non_import1  (#not-has-type? @_non_import1  import_statement import_from_statement future_import_statement)
+  . (_)  @_non_import1  (#not-kind-eq? @_non_import1  import_statement import_from_statement future_import_statement)
   ; However, don't match if followed by another import statement,
   ; to ensure the capture group is maximial and avoid nested foldings.
-  . (_)? @_non_import2  (#not-has-type? @_non_import2  import_statement import_from_statement future_import_statement)
+  . (_)? @_non_import2  (#not-kind-eq? @_non_import2  import_statement import_from_statement future_import_statement)
 
   (#make-range! "fold" @_start @_end)
 )
