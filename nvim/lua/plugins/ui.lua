wookayin/dotfiles

commit fbfc7a2d2ed4e64e9008300b5e9d5a4f5502fa27

vim(neotree): Allow running cmd `:Neotree` before lazy loading
e.g. `nvim -c "Neotree"`

nvim/lua/plugins/ui.lua
@@ -70,6 +70,7 @@ return {
       if vim.tbl_contains(vim.tbl_map(vim.fn.isdirectory, vim.fn.argv()), 1) then return nil
       else return 'VeryLazy' end
     end)(),
+    cmd = { 'Neotree' },
     init = function() vim.g.neo_tree_remove_legacy_commands = 1; end,
     config = require('config.neotree').setup_neotree,
   };

commit 025a2b3a0cc243aeb21e499f85f5f5f7488a1aa2

vim(neotree): Fix neotree not hijacking startup argument directories
This is a regression since fbfc7a2d. Neotree was not being loaded
when the startup argument contains a directory.

nvim/lua/plugins/ui.lua
@@ -65,12 +65,12 @@ return {
     branch = 'main',
     version = '>=3.12',
     event = (function()
-      -- If any of the startup argument is a directory,
+      -- If any of the startup argument is a directory (e.g., "nvim ./"),
       -- we don't lazy-load neotree so it can hijack netrw.
-      if vim.tbl_contains(vim.tbl_map(vim.fn.isdirectory, vim.fn.argv()), 1) then return nil
+      if vim.tbl_contains(vim.tbl_map(vim.fn.isdirectory, vim.fn.argv()), 1) then return 'VimEnter'
       else return 'VeryLazy' end
     end)(),
-    cmd = { 'Neotree' },
+    cmd = { 'Neotree' },  -- support "nvim -c Neotree"
     init = function() vim.g.neo_tree_remove_legacy_commands = 1; end,
     config = require('config.neotree').setup_neotree,
   };