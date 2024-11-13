284c9827db87427e379ab2da5a11b2ee78f9a0dd
Jongwook Choi <wookayin@gmail.com>
2024-11-12 19:30:59 -0500

    vim(highlight): Highlight "--flags" in bash array assignments
    Highlight flags in cmdline-like array, e.g.:
    ```bash
    cmd=(
        bin/cmd --foo --bar="1"
    ); "${cmd[@]}"
    ```
    
    as well as:
    ```bash
    bin/cmd --foo --bar="1"
    ```

--- nvim/after/queries/bash/highlights.scm
+++ nvim/after/queries/bash/highlights.scm
@@ -4,11 +4,27 @@
 ; $(subcommand ...) <- do NOT highlight as string (red)
 (command) @command
 
+; Highlight "--flags" in bash commands
 ; In addition to @variable.parameter highlight,
 ; we mark as @variable.parameter.flag for CLI "--options" or "--flag"
 (command
   argument: [
-             (word) @variable.parameter.flag
-             (concatenation (word) @variable.parameter.flag)
-             ]
+    (word) @variable.parameter.flag
+    (concatenation (word) @variable.parameter.flag)
+  ]
+  (#lua-match? @variable.parameter.flag "^%-%-?%w+"))
+
+; also in array variables, e.g. cmd=(foo --bar="1" --baz)
+(variable_assignment
+  (array
+    (concatenation
+      (word) @none  ; override (concatenation (word) @string), e.g. foobar="?"
+    )))
+(variable_assignment
+  value: (array
+    [
+      (word) @variable.parameter.flag
+      (concatenation (word) @variable.parameter.flag)
+    ]
+  )
   (#lua-match? @variable.parameter.flag "^%-%-?%w+"))
