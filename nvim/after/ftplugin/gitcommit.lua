f2ae4a65ce81269730d0b015b1a7ea1b6aaf7882
Jongwook Choi <wookayin@gmail.com>
2025-01-22 15:54:33 -0500

    vim: turn on spell checking for gitcommit
    See :help hl-SpellBad

--- nvim/after/ftplugin/gitcommit.lua
+++ nvim/after/ftplugin/gitcommit.lua
@@ -4,3 +4,6 @@
 require("config.treesitter").ensure_parsers_installed { "gitcommit", "diff" }
 require("config.treesitter").setup_highlight("gitcommit")
 vim.bo.syntax = "ON"  -- enable vim syntax, e.g. gitcommitBlank are useful
+
+-- Spell Checking
+vim.wo.spell = true
