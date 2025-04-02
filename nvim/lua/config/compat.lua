510985af6bb3deb4f10a584b97e0a8266d3cec04
Jongwook Choi <wookayin@gmail.com>
2025-04-02 15:01:10 -0400

    vim(compat): compat bridge for vim.hl and vim.tbl_flatten
    neovim 0.11 has been released.
    
    - vim.hl => does not exist in neovim 0.10, but we should use vim.hl
    - `vim.tbl_flatten` is a deprecated API (warnings on neovim 0.12-dev),
      use it until all the plugins fully get rid of its use.

--- nvim/lua/config/compat.lua
+++ nvim/lua/config/compat.lua
@@ -1,6 +1,6 @@
 -- Compatibility layer for vim lua APIs
--- Backport core lua APIs that does not exist in neovim 0.8.0 (from dev version)
--- see also
+-- Backport core lua APIs that does not exist in older versions of neovim (from stable/dev version)
+-- See also
 --   :HelpfulVersion
 ---@diagnostic disable: deprecated
 
@@ -15,4 +15,16 @@ end
 -- for nvim >= 0.11, against deprecated warnings (until plugins catch up)
 if has('nvim-0.11') then
   vim.tbl_islist = vim.islist
+end
+
+-- nvim 0.11: vim.highlight => vim.hl (does not exist in neovim 0.10)
+if vim.hl == nil then
+  vim.hl = vim.highlight
+end
+
+-- deprecated in nvim 0.12, use the same behavior (exclude false and nil)
+if has('nvim-0.12') then
+  vim.tbl_flatten = function(t)
+    return vim.iter(t):filter(function(truthy) return truthy end):flatten(math.huge):totable()
+  end
 end
