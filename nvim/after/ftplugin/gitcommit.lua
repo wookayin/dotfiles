06836d94dab8fec2bedd1c09fb39a33d37926383
Jongwook Choi <wookayin@gmail.com>
2026-02-16 12:13:12 -0500

    vim: workaround treesitter foldupdate error on :GCommit
    One common error happening these days (for nightly nvim) on :GCommit is
    a "invalid bot" error around treesitter fold: vim._foldupdate().
    
    An inspection shows that in some cases we have `srow == erow` (which
    seems valid, representing an empty range), but I don't get why the error
    occurs. This is definitely a bug of NVIM upstream.
    
    One quick workaround is to disable treesitter foldexpr for gitcommit
    buffers.

--- nvim/after/ftplugin/gitcommit.lua
+++ nvim/after/ftplugin/gitcommit.lua
@@ -7,3 +7,6 @@ vim.bo.syntax = "ON"  -- enable vim syntax, e.g. gitcommitBlank are useful
 
 -- Spell Checking
 vim.wo.spell = true
+
+-- Workaround for "invalid bot" errors in vim._foldupdate(), when running :GCommit
+vim.wo.foldmethod = 'manual'
