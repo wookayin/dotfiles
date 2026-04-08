4fd51e883de4f5fb5d8e7da9814739d00e5fa09b
Jongwook Choi <wookayin@gmail.com>
2026-04-07 17:51:58 -0700

    zsh: use CTRL-z as shortcut to fzf + 'z' (autojump to recent dirs)

--- zsh/zsh.d/fzf-widgets.zsh
+++ zsh/zsh.d/fzf-widgets.zsh
@@ -6,3 +6,7 @@
 
 # More Shortcuts
 bindkey '^ ' fzf-file-widget          # Ctrl-SPACE, Ctrl-T
+
+# ctrl-z (as well as alt-c): fzf for 'z' (recent directories).
+# See ~/.zsh/zsh.d/envs.zsh for ALT-C configurations.
+bindkey '^z' fzf-cd-widget
