cbd2948b2e4c4c7171bfcbd4932e26f87b57cf8e
Jongwook Choi <wookayin@gmail.com>
2026-04-08 20:07:50 -0700

    zsh(fzf): add custom tab completion for `v` and `kill` command
    - `kill <TAB>` => launches the `pid.fzf` picker for inserting PID
    - `v <TAB>` => launches fzf launcher for file completion
      (this is experimental, so I'm not applying for `vim <TAB>` for now)

--- zsh/zsh.d/fzf-widgets.zsh
+++ zsh/zsh.d/fzf-widgets.zsh
@@ -10,3 +10,42 @@ bindkey '^ ' fzf-file-widget          # Ctrl-SPACE, Ctrl-T
 # ctrl-z (as well as alt-c): fzf for 'z' (recent directories).
 # See ~/.zsh/zsh.d/envs.zsh for ALT-C configurations.
 bindkey '^z' fzf-cd-widget
+
+
+# Advanced, customized <TAB> (^I) completion through fzf widgets
+# This overrides the default 'fzf-complete' widget (see ~/.fzf/shell/completion.zsh)
+zle -A fzf-complete _orig_fzf-complete 2>/dev/null ||
+  zle -A expand-or-complete _orig_fzf-complete
+
+fzf-complete-custom() {
+  # {v,vim,nvim} <TAB>: use fzf file finder (like Ctrl-T), with the trailing word as initial query
+  # TODO: This is too experimental, apply to `v` only, we can apply to `vim` later
+  if [[ "$LBUFFER" =~ ^[[:space:]]*(v)[[:space:]] && ! "$LBUFFER" =~ -[a-zA-Z0-9]*$ ]]; then
+    # TODO: detect if the current completion (for fallback) is 'file' completion, and invoke fzf widget only when it's the case?
+    # e.g. vim +<TAB>, vim -c 'lua<TAB>' should not invoke fzf completion
+    local query="${LBUFFER##* }"   # Extract the last word as query
+    LBUFFER="${LBUFFER%$query}"    # Strip the query from LBUFFER
+    local FZF_CTRL_T_OPTS="$FZF_CTRL_T_OPTS --query='${query:-}'"
+    zle fzf-file-widget
+
+  # kill <TAB>
+  elif [[ "$LBUFFER" =~ ^[[:space:]]*kill[[:space:]] && ! "$LBUFFER" =~ -[a-zA-Z0-9]*$ ]]; then
+    # Like _fzf_complete_kill, but using ~/.dotfiles/bin/pid.fzf
+    local query="${LBUFFER##* }"  # Extract the last word as query
+    LBUFFER="${LBUFFER%$query}"   # Strip the query from LBUFFER
+    local pid
+    pid=$("pid.fzf" --no-tmux --no-border --no-footer --prompt="Pick process to kill> " --query="$query" < /dev/tty 2>/dev/tty)
+    if [[ -n "$pid" ]]; then
+      LBUFFER="${LBUFFER}${pid//$'\n'/ }"
+    fi
+
+    zle reset-prompt
+
+  # Fallback to the default <Tab> completion (fzf -> expand-or-complete)
+  else
+    zle _orig_fzf-complete
+  fi
+}
+
+zle -N fzf-complete-custom
+bindkey '^I' fzf-complete-custom
