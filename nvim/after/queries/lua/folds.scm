936def1ad2f7df25d82b69dde752cb67ed8c90cd
Jongwook Choi <wookayin@gmail.com>
2026-04-18 00:25:25 -0700

    vim(treesitter): fix folding of consecutive luadoc regions
    The `#make-range!` directive is gone, and NVIM now fully supports
    quantification operators so we no longer need these hacky queries.
    
    See https://tree-sitter.github.io/tree-sitter/using-parsers/queries/2-operators.html#quantification-operators

--- nvim/after/queries/lua/folds.scm
+++ nvim/after/queries/lua/folds.scm
@@ -6,34 +6,14 @@
 ) @fold
 
 
-; 🚧 Fold a maximally consecutive lines of luadoc comments.
-; NOTE: This query is a hack until a upstream bug is fixed: neovim/neovim#17060
-; where (node)+ quantifiers are not captured properly as a single group.
-;   see potential fix: neovim/neovim#17099 -> neovim/neovim#24738
-;   also relevant: tree-sitter/tree-sitter#2468
+; Fold a maximally consecutive lines of luadoc comments.
 (
-  ; The following somehow doesn't work for a very long comments. Might be a bug?
-  ; (_) @_non_comment (#not-kind-eq? @_non_comment comment)
+  (comment)+ @fold
+  .
   [
     (function_declaration)
     (assignment_statement)
     (variable_declaration)
     (function_call)
-    (do_statement)
-    (while_statement)
-    (if_statement)
-    (for_statement)
   ]
-  ; luadoc region with annotations
-  . (comment) @_start
-  . (comment)*
-  . (comment) @_end
-  ; function, variable, etc. that is being annotated
-  . [
-      (function_declaration)
-      (assignment_statement)
-      (variable_declaration)
-      (function_call)
-    ]
-  (#make-range! "fold" @_start @_end)
 )
