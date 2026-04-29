092f77b79d4d27be270f8ba0b5371898a854620c
Jongwook Choi <wookayin@gmail.com>
2026-04-28 20:18:36 -0700

    vim(ftplugin/query): fix ExtendsOpen for new nvim-treesitter
    Since nvim-treesitter v1.0, the path to query files have changed from
    the vim plugin directory to `~/.local/share/nvim/treesitter/queries`.
    The `:ExtendsOpen` commands should be fixed to catch up the change.

--- nvim/after/ftplugin/query.lua
+++ nvim/after/ftplugin/query.lua
@@ -14,13 +14,13 @@ vim.bo.sw = 2
 vim.api.nvim_buf_create_user_command(0, 'ExtendsOpen', function(opts)
   local path = vim.fn.expand("%:p")  --[[ @as string ]]
 
-  -- e.g. "queries/lua/highlights.scm"
-  local rpath = path:match("nvim/after/(.-)$")
+  -- e.g. "lua/highlights.scm"
+  local rpath = path:match("nvim/after/queries/(.-)$")
   if rpath then
-    assert(os.getenv('VIMPLUG'))
-    local target = vim.fn.expand("$VIMPLUG/nvim-treesitter/" .. rpath)
+    local base = require("nvim-treesitter.config").get_install_dir("queries")  -- v1.0
+    local target = vim.fn.expand(vim.fs.joinpath(base, rpath))
     vim.cmd.edit { args = { target } }
   else
-    vim.api.nvim_err_writeln("Not a nvim config query file.")
+    vim.api.nvim_echo({ { "Not a nvim config query file.", 'ErrorMsg' } }, true, {})
   end
 end, { desc = "Open the query file in nvim-treesitter which this ';;extends' from)" })
