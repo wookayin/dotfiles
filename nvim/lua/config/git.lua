wookayin/dotfiles

commit 160a06277f8164f2d1ce5d84472b764ba647751c

vim(git): GDiffTerm doesn't need to pipe into less, to enable git-delta
`git diff` (on a terminal) will involve git-delta as a pager per git
config, so piping `less -+F` is not necessary. With this fix, git-delta
highlight will be enabled for git diff.

nvim/lua/config/git.lua
@@ -30,8 +30,7 @@ function M.setup_fugitive()
   vim.api.nvim_create_user_command("GDiffTerm", function(e)
     local args = vim.fn.expandcmd(vim.trim(e.args))  -- supports '%'
     local cmd = "git diff --color " .. args
-    -- Disable -F (exit on EOF) because we will autoclose the floaterm
-    vim.fn['floaterm#new'](0, cmd .. " | less -+F", vim.empty_dict(), {
+    vim.fn['floaterm#new'](0, cmd, vim.empty_dict(), {
       name = 'git', autoclose = 1,
       title = " " .. cmd .. " ",
     })

commit 69e56cf372a0f4a6ffc96c5aff43d79b70973a5a

vim(git): apply LESS option for GDiffTerm
Regression in 160a06277f; when the diff is too small, the default pager
(less) will instantly quit. Instead of piping with `less -+F`, we use
the `$LESS` env variable to control this behavior so that `git-delta`
can still work.

nvim/lua/config/git.lua
@@ -29,7 +29,7 @@ function M.setup_fugitive()
   command_alias "gdc" "GDiffTerm --cached"
   vim.api.nvim_create_user_command("GDiffTerm", function(e)
     local args = vim.fn.expandcmd(vim.trim(e.args))  -- supports '%'
-    local cmd = "git diff --color " .. args
+    local cmd = "LESS=-+F git diff --color " .. args
     vim.fn['floaterm#new'](0, cmd, vim.empty_dict(), {
       name = 'git', autoclose = 1,
       title = " " .. cmd .. " ",
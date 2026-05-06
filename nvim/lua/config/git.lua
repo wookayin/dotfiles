69e56cf372a0f4a6ffc96c5aff43d79b70973a5a
Jongwook Choi <wookayin@gmail.com>
2026-05-06 15:56:58 -0700

    vim(git): apply LESS option for GDiffTerm
    Regression in 160a06277f; when the diff is too small, the default pager
    (less) will instantly quit. Instead of piping with `less -+F`, we use
    the `$LESS` env variable to control this behavior so that `git-delta`
    can still work.

--- nvim/lua/config/git.lua
+++ nvim/lua/config/git.lua
@@ -29,7 +29,7 @@ function M.setup_fugitive()
   command_alias "gdc" "GDiffTerm --cached"
   vim.api.nvim_create_user_command("GDiffTerm", function(e)
     local args = vim.fn.expandcmd(vim.trim(e.args))  -- supports '%'
-    local cmd = "git diff --color " .. args
+    local cmd = "LESS=-+F git diff --color " .. args
     vim.fn['floaterm#new'](0, cmd, vim.empty_dict(), {
       name = 'git', autoclose = 1,
       title = " " .. cmd .. " ",
