d8af609eae1f9de3cac93f1ab8b4c891c2bda70a
Jongwook Choi <wookayin@gmail.com>
2026-05-31 13:04:53 -0700

    vim(lsp/ruff): default lint rule with customized diagnostic severity
    The existing ruff-server configs (args = '--ignore ...') were not
    working for ruff LSP. We fix it to use the correct lsp configs.
    
    We also add a custom config to override diagnostic severity; ruff
    reports most of the lint violations as WARN level, but some are better
    surfaced at HINT or INFO level. Here we specify some lint rules to be
    presented as HINT level, e.g. I001, flake8, refactoring suggestions.

--- nvim/after/lsp/ruff.lua
+++ nvim/after/lsp/ruff.lua
@@ -1,27 +1,53 @@
 -- extends $VIMPLUG/nvim-lspconfig/lsp/ruff.lua
 
--- https://github.com/astral-sh/ruff-lsp#settings
--- https://github.com/astral-sh/ruff-lsp/blob/main/ruff_lsp/server.py
--- Note: use pyproject.toml to configure ruff per project.
+-- The "ruff" Language Server
+-- https://docs.astral.sh/ruff/editors/settings/
 local init_options = {
   settings = {
     fixAll = true,
     organizeImports = false, -- in favor of Conform (:Format ruff_organize_imports)
-    -- extra CLI arguments
-    -- https://docs.astral.sh/ruff/configuration/#command-line-interface
+
+    -- NOTE: The lint/format rules for the "ruff LSP" here are meant to be a sensible default
+    -- setting in the editor level; the ruff CLI (`ruff check`) still may give linting errors.
+    -- Per-project lint/format config should be configured in pyproject.toml.
     -- https://docs.astral.sh/ruff/rules/
-    args = {
-      "--preview", -- Use experimental features
-      "--ignore", table.concat({
+    lint = {
+      preview = true, -- Use experimental features by default
+      ignore = {
+        "C408", -- unnecessary-collection-call  e.g. dict(foo=bar)
         "E111", -- indentation-with-invalid-multiple
         "E114", -- indentation-with-invalid-multiple-comment
         "E402", -- module-import-not-at-top-of-file
         "E501", -- line-too-long
         "E702", -- multiple-statements-on-one-line-semicolon
         "E731", -- lambda-assignment
-        "F401", -- unused-import  (note: should be handled by pyright as 'hint')
-      }, ','),
+        -- These are still checked by ruff, but should be HINT rather than WARN (see below)
+        -- "F401", -- unused-import
+        -- "F841", -- unused-variable
+      },
+      -- [Override diagnostic severity]
+      -- Ruff reports most of the lint violations (exceptions are syntax errors) as WARN level,
+      -- but some lint rules are better surfaced at HINT/INFO level.
+      -- This is a field recognized not by the ruff LSP, but by our custom diagnostics LSP handlers.
+      _severity_overrides = { ---@type table<string, lsp.DiagnosticSeverity>
+        ["I001"] = vim.diagnostic.severity.HINT,  -- unsorted-imports
+
+        ["F401"] = vim.diagnostic.severity.WARN,  -- unused-import
+        ["F841"] = vim.diagnostic.severity.WARN,  -- unused-variable
+        ["RUF059"] = vim.diagnostic.severity.WARN,  -- unused-unpacked-variable
+
+        ["C4"] = vim.diagnostic.severity.HINT,  -- flake8-comprehensions
+        ["PLC"] = vim.diagnostic.severity.HINT,  -- pylint conventions
+        ["PLR"] = vim.diagnostic.severity.HINT,  -- pylint refactors
+        ["PLW"] = vim.diagnostic.severity.WARN,  -- pylint warnings
+        ["UP"] = vim.diagnostic.severity.HINT,  -- pyupgrade
+      },
     },
+    format = {
+      preview = true,
+    },
+    -- Configuration files (e.g. pyproject.toml) takes priority over editor (lspconfig) settings
+    configurationPreference = "filesystemFirst",
   },
 }
 
@@ -35,9 +61,48 @@ local on_init = function(client, _)
   end
 end
 
+
+---@param code string|integer
+---@return lsp.DiagnosticSeverity|nil
+local function resolve_severity(code)
+  code = tostring(code)
+  local severity_overrides = init_options.settings.lint._severity_overrides
+  if severity_overrides[code] then return severity_overrides[code] end
+  for prefix, severity in pairs(severity_overrides) do
+    if vim.startswith(code, prefix) then return severity end
+  end
+  return nil
+end
+---@param diags lsp.Diagnostic[]
+local function patch_severities(diags)
+  for _, diag in ipairs(diags) do
+    local new_severity = resolve_severity(diag.code)
+    if new_severity then
+      diag.severity = new_severity
+    end
+  end
+end
+
+
 return {
   init_options = init_options,
   on_init = on_init,
+  handlers = { ---@type table<string, lsp.Handler>
+    ---@param params lsp.PublishDiagnosticsParams
+    ["textDocument/publishDiagnostics"] = function(err, params, ctx, config)  -- notification
+      if params and params.diagnostics then
+        patch_severities(params.diagnostics)
+      end
+      return vim.lsp.handlers["textDocument/publishDiagnostics"](err, params, ctx, config)
+    end,
+    ---@param result lsp.DocumentDiagnosticReport
+    ["textDocument/diagnostic"] = function(err, result, ctx, config)  -- response
+      if result and result.items then
+        patch_severities(result.items)
+      end
+      return vim.lsp.handlers["textDocument/diagnostic"](err, result, ctx, config)
+    end,
+  },
   capabilities = {
     general = {
       -- pyright uses utf-16, and ruff uses utf-8 by default.
