wookayin/dotfiles

commit 0ca8c48fd3d61224bfcb7eac608735d74bc46da3

vim(utils): refactor path_utils.project_root()
- Consolidate `find_project_root()` => `project_root(path|buf?, marker)`
- Add `buf_dir(buf?)`

nvim/after/ftplugin/tex.lua
@@ -8,14 +8,14 @@ require('config.treesitter').setup_highlight('latex')
 -- Configure :Build
 require('config.tex').setup_compiler_commands()
 
-local path = require("utils.path_utils")
-vim.b.project_root = path.find_project_root({ 'Makefile', '.latexmkrc', '.git' }, { buf = 0 })
+local path_utils = require("utils.path_utils")
+vim.b.project_root = path_utils.project_root(0, { 'Makefile', '.latexmkrc', '.git' })
 if vim.b.project_root then vim.cmd.lcd(vim.b.project_root) end
 
 --[[ Make and build support ]]
-if vim.fn.filereadable(path.join(vim.b.project_root, "Makefile")) > 0 then
+if vim.fn.filereadable(vim.fs.join(vim.b.project_root, "Makefile")) > 0 then
   vim.opt_local.makeprg = 'make'
-elseif vim.fn.filereadable(path.join(vim.fn.expand("%:p:h"), "Makefile")) > 0 then
+elseif vim.fn.filereadable(vim.fs.join(vim.fn.expand("%:p:h"), "Makefile")) > 0 then
   vim.opt_local.makeprg = 'make'
 else
   vim.opt_local.makeprg = '(latexmk -pdf -pdflatex="pdflatex -halt-on-error -interaction=nonstopmode -file-line-error -synctex=1" "%:r" && latexmk -c "%:r")'

nvim/after/ftplugin/bib.lua
@@ -9,6 +9,6 @@ require('config.tex').setup_compiler_commands()
 require('config.treesitter').ensure_parsers_installed { 'bibtex' }
 
 -- Build path
-local path = require("utils.path_utils")
-vim.b.project_root = path.find_project_root({ 'Makefile', '.latexmkrc', '.git' }, { buf = 0 })
+local path_utils = require("utils.path_utils")
+vim.b.project_root = path_utils.project_root(0, { 'Makefile', '.latexmkrc', '.git' })
 if vim.b.project_root then vim.cmd.lcd(vim.b.project_root) end

nvim/after/ftplugin/lua.lua
@@ -27,7 +27,7 @@ if is_test then
   vim.api.nvim_buf_create_user_command(0, "Build", "echon ':Test' | Test", {})
   vim.api.nvim_buf_create_user_command(0, "Output", "TestOutput", {})
 
-  local project_root = require("utils.path_utils").find_project_root({ ".git" }) ---@type string?
+  local project_root = require("utils.path_utils").project_root(0, { ".git" }) ---@type string?
   local filename = assert(vim.fn.bufname('%'))  -- e.g. test/functional/lua/api_spec.lua
 
   -- exception: neovim tests do not use plenary-busted but the original busted

nvim/lua/config/formatting.lua
@@ -191,7 +191,8 @@ local find_project_root = function(buf)
   local path = vim.api.nvim_buf_call(buf, function() return vim.fn.expand("%:p") end)
   local project_root = (
     vim.b[buf].project_root or
-    require("utils.path_utils").find_project_root({ ".git" }, { path = path }))
+    require("utils.path_utils").project_root(path, { ".git" })
+  )
 
   project_root = project_root and vim.fn.resolve(project_root) or nil
   return project_root

nvim/lua/utils/path_utils.lua
@@ -3,31 +3,49 @@
 
 local M = {}
 
-function M.join(...)
-  return vim.fn.resolve(table.concat(vim.tbl_flatten {...}, '/'))
+--- Get the directory of a buffer's file path, or nil if the buffer has no associated file.
+--- @param buf integer|nil buffer number, defaults to current buffer
+--- @return string|nil
+function M.buf_dir(buf)
+  buf = buf or vim.api.nvim_get_current_buf()
+  vim.validate('buf', buf, 'number', true)
+
+  -- special case: neo-tree, use the explorer's root path directly
+  if vim.bo[buf].filetype == 'neo-tree' then
+    local neotree_path = require("config.neotree").get_path()
+    if neotree_path then
+      return neotree_path
+    end
+  end
+
+  if vim.bo[buf].buftype ~= "" then
+    return nil
+  end
+  local bufname = vim.api.nvim_buf_get_name(buf)
+  if bufname == "" then
+    return nil
+  end
+  return vim.fs.dirname(bufname)
 end
 
----@param marker_patterns string[]
----@param opts table? { buf = ... } or { path = ... } or <buf> or <path>
+---@param marker_patterns string[]|nil
+---@param start_path string|integer|nil  defaults to cwd. accepts path or bufnr
 ---@return string|nil
-function M.find_project_root(marker_patterns, opts)
-  local start_path
-  if opts == nil or type(opts.buf) == "number" then
-    local buf = opts and opts.buf or 0
-    start_path = vim.fs.dirname(vim.api.nvim_buf_get_name(buf))
-  elseif type(opts) == "table" and opts.path then
-    start_path = opts.path
-  elseif type(opts) == "string" then
-    start_path = opts
-  else
-    error("Unknown type: " .. vim.inspect(opts))
+function M.project_root(start_path, marker_patterns)
+  marker_patterns = marker_patterns or { '.git' }
+  if type(start_path) == 'number' then
+    start_path = M.buf_dir(start_path)
+  end
+  ---@cast start_path string|nil
+  if start_path == nil then
+    start_path = vim.fn.getcwd()
   end
 
   local marker = vim.fs.find(
     marker_patterns,
     {
       upward = true, stop = vim.loop.os_homedir(), limit = 1,
-      path = vim.fn.fnamemodify(start_path, ":p"),
+      path = vim.fs.abspath(start_path),
     })[1]
 
   -- fnamemodify: don't use :p, it addes trailing slashes when matched .git

commit 2a582805a26814e25ffb7cc462dab2cabe13480e

vim(ftplugin/tex): fix broken API usage, vim.fs.joinpath
A small regression in 0ca8c48f

nvim/after/ftplugin/tex.lua
@@ -13,9 +13,9 @@ vim.b.project_root = path_utils.project_root(0, { 'Makefile', '.latexmkrc', '.gi
 if vim.b.project_root then vim.cmd.lcd(vim.b.project_root) end
 
 --[[ Make and build support ]]
-if vim.fn.filereadable(vim.fs.join(vim.b.project_root, "Makefile")) > 0 then
+if vim.fn.filereadable(vim.fs.joinpath(vim.b.project_root, "Makefile")) > 0 then
   vim.opt_local.makeprg = 'make'
-elseif vim.fn.filereadable(vim.fs.join(vim.fn.expand("%:p:h"), "Makefile")) > 0 then
+elseif vim.fn.filereadable(vim.fs.joinpath(vim.fn.expand("%:p:h"), "Makefile")) > 0 then
   vim.opt_local.makeprg = 'make'
 else
   vim.opt_local.makeprg = '(latexmk -pdf -pdflatex="pdflatex -halt-on-error -interaction=nonstopmode -file-line-error -synctex=1" "%:r" && latexmk -c "%:r")'