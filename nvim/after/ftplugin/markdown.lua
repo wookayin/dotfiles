f76027ee26a12eebbb3decf3e32de07e10cb1d32
Jongwook Choi <wookayin@gmail.com>
2026-06-20 02:02:59 -0700

    vim(markdown): Remove `:Grip`, add `:Preview`
    Grip sends the markdown file to Github API server. Due to a privacy and
    security concern, grip is discouraged to use.
    
    A simple alternative is preview-markdown (but it's not perfect either).

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -21,27 +21,35 @@ if vim.bo.buftype == "" then
   setlocal.spell = true
 end
 
--- GFM markdown preview using grip
--- (pip install grip)
-vim.api.nvim_buf_create_user_command(0, 'Grip', function(opts)
-  local win = vim.api.nvim_get_current_win()
-  local exitcode = nil
-  vim.cmd [[ botright 7new ]]
-  vim.cmd [[ setlocal winfixheight ]]
-  vim.fn.termopen({ "grip", vim.fn.expand('%'), "0.0.0.0" }, {
-    on_exit = function(job_id, data, event)
-      exitcode = data
-    end,
-  })
-  vim.api.nvim_set_current_win(win)
-  vim.cmd.stopinsert()  -- workaround for autocmd+terminal bug
 
-  if vim.ui.open and os.getenv('SSH_CONNECTION') == nil then
-    vim.defer_fn(function()
-      if exitcode == nil or exitcode == 0 then
-        vim.ui.open('http://localhost:6419/')
+-- Markdown preview using markdown-preview
+-- Install: npm install -g @mryhryki/markdown-preview
+vim.api.nvim_buf_create_user_command(0, 'Preview', function(opts)
+  local function run_preview(cmd)
+    local win = vim.api.nvim_get_current_win()
+    local exitcode = nil
+    vim.cmd [[ botright 7new ]]
+    vim.cmd [[ setlocal winfixheight ]]
+    vim.fn.termopen(cmd, {
+      on_exit = function(job_id, data, event)
+        exitcode = data
+      end,
+    })
+    vim.api.nvim_set_current_win(win)
+    vim.cmd.stopinsert()  -- workaround for autocmd+terminal bug
+  end
+
+  local filepath = vim.fn.fnamemodify(vim.fn.expand('%:p'), ':~:.')
+  if false and vim.fn.executable('markdown-preview') == 1 then
+    run_preview({ 'markdown-preview', '-f', filepath })
+  else
+    vim.ui.select({ 'Yes', 'No' }, {
+      prompt = '`markdown-preview` CLI not found. Run via npx @mryhryki/markdown-preview?',
+    }, function(choice)
+      if choice and choice:lower() == 'y' then
+        run_preview({ 'npx', '@mryhryki/markdown-preview', '-f', filepath })
       end
-    end, 200)
+    end)
   end
 end, { nargs = 0 })
 
